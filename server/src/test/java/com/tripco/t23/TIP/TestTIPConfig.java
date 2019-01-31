4a371f5f808a0018fa19a96fef23722e82234998
Timothy Rooney <tprooney@winter-park.cs.colostate.edu>
2019-01-30 18:01:27 -0700

    Modified TIPConfig and its test to reflect team name; Modified index.html to reflect team name.

--- server/src/test/java/com/tripco/t23/TIP/TestTIPConfig.java
+++ server/src/test/java/com/tripco/t23/TIP/TestTIPConfig.java
@@ -33,7 +33,7 @@ public class TestTIPConfig {
   @Test
   public void testServerName() {
     String name = conf.getServerName();
-    assertEquals("config name", "t## team name", name);
+    assertEquals("config name", "t23 Byte Me", name);
   }
 
   @Test
